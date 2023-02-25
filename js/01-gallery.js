a33eec5fd6c1db6bf69b680165f262a72d257667
Олена Пестова <nowalena9@gmail.com>
2023-02-25 14:49:12 +0200

    task-01 done

---
+++ js/01-gallery.js
@@ -0,0 +1,41 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+//console.log(galleryItems);
+//створимо масив розмітки де кожен ел-т це [<a><img/>....</a>] і зєднаємо все в 1 рядок
+let markUp = galleryItems
+    .map(({ preview, original, description }) => 
+    `<div uk-lightbox class="gallery__item"> 
+     <a class="gallery__link" href = ${original} >
+        <img
+        class="gallery__image"
+        src=${preview}
+        data-source=${original}
+        alt=${description}
+        />
+     </a>
+     </div>
+    ` )
+    .join("");
+const galleryRef = document.querySelector(".gallery");
+//console.log("markUp ",markUp);
+galleryRef.innerHTML = markUp;
+
+galleryRef.addEventListener("click", showOriginal);
+let instance = {};
+function showOriginal(event) {
+    event.preventDefault();
+    if (!event.target.classList.contains("gallery__image")){
+         return; 
+    }
+    // import * as basicLightbox from 'basiclightbox'
+    instance = basicLightbox.create(`
+    <img src=${event.target.dataset.source} width="800" height="600">`);
+    instance.show();
+       
+};
+galleryRef.addEventListener("keydown", (event) => {
+    //console.log(event.code);
+    if (event.code === "Escape") {
+        instance.close();
+    }
+});
